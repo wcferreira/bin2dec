04f9c48c467a20e0d6fc96625678b1187856a8d8
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-22 15:52:26 -0300

    test: returns 1 when 1 passed in

--- test/bin2dec/converter_test.clj
+++ test/bin2dec/converter_test.clj
@@ -6,3 +6,6 @@
   (let [ret-val (conv/bin2dec "0")]
     (is (= 0 ret-val) "It should return 0 when \"0\" is passed in")))
 
+(deftest test-returns-one-when-one-passed-in
+  (let [ret-val (conv/bin2dec "1")]
+    (is (= 1 ret-val) "It should return 1 when \"1\" is passed in")))
