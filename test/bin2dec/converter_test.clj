1917db35aef4f824cdfa461753457385c93ad29f
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-22 15:47:43 -0300

    test: returns 0 when 0 passed in

--- test/bin2dec/converter_test.clj
+++ test/bin2dec/converter_test.clj
@@ -1,2 +1,8 @@
 (ns bin2dec.converter-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [bin2dec.converter :as conv]))
+
+(deftest test-returns-zero-when-zero-passed-in
+  (let [ret-val (conv/bin2dec "0")]
+    (is (= 0 ret-val) "It should return 0 when \"0\" is passed in")))
+
