679b579d366f990196d650434231123c0f526438
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-22 16:27:31 -0300

    refact: change bin2dec function to convert any binary number into a decimal one

--- src/bin2dec/converter.clj
+++ src/bin2dec/converter.clj
@@ -1,6 +1,30 @@
 (ns bin2dec.converter)
 
-(defn bin2dec
+(defn string->vector
+  "Convert a string representing a binary into a vector."
   [bin]
-  (Integer. bin))
+  (->> (seq bin)
+       (map int)
+       (mapv #(- % 48))))
 
+(defn pow
+  "Calculates the power of a number"
+  [base exp]
+  (Math/round (Math/pow base exp)))
+
+;; Binary power
+(def pow2 (partial pow 2))
+
+(defn bin2dec
+  "Convert a binary number into a decimal number"
+  [bin-seq]
+  (loop [index 0
+         result 0
+         col (->> bin-seq
+                  string->vector
+                  reverse)]
+    (if (empty? col)
+      result
+      (recur (inc index)
+             (+ result (* (first col) (pow2 index)))
+             (rest col)))))
